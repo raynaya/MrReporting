f2922fbd8f00e387cdb5bc85e43ae197b520dadb
raynaya <ayan_ray4u@yahoo.co.in>
2012-04-07 18:17:58 +0530

    Just another small change

---
+++ sql files/pharma_database_masters.sql
@@ -0,0 +1,229 @@
+insert into statemaster values('Delhi');
+insert into statemaster values('Maharastha');
+insert into statemaster values('TamilNadu');
+insert into statemaster values('Bihar');
+insert into statemaster values('MadhyaPradesh');
+insert into statemaster values('West Bengal');
+
+insert into hqmaster values('Delhi','HQDelhi01');
+insert into hqmaster values('Maharastha','HQMaharastha01');
+insert into hqmaster values('TamilNadu','HQTamilNadu01');
+insert into hqmaster values('Bihar','HQBihar01');
+insert into hqmaster values('MadhyaPradesh','HQMadhyaPradesh01');
+insert into hqmaster values('West Bengal','HQWest Bengal01');
+
+
+insert into citymaster values('Delhi','HQDelhi01','KarolBagh');
+insert into citymaster values('Delhi','HQDelhi01','Kalkaji');
+insert into citymaster values('Maharastha','HQMaharastha01','Pune');
+insert into citymaster values('Maharastha','HQMaharastha01','Mumbai');
+insert into citymaster values('Maharastha','HQMaharastha01','Aurangabad');
+insert into citymaster values('TamilNadu','HQTamilNadu01','Chennai');
+insert into citymaster values('TamilNadu','HQTamilNadu01','Erode');
+insert into citymaster values('Bihar','HQBihar01','Patna');
+insert into citymaster values('Bihar','HQBihar01','Samastipur');
+insert into citymaster values('MadhyaPradesh','HQMadhyaPradesh01','Bhupal');
+insert into citymaster values('MadhyaPradesh','HQMadhyaPradesh01','Ujjain');
+insert into citymaster values('West Beagal','HQWest Bengal01','Kolkata');
+insert into citymaster values('West Bengal','HQWest Bengal01','Siliguri');
+insert into citymaster values('West Bengal','HQWest Bengal01','Durgapur');
+
+insert into areamaster values('Delhi','HQDelhi01','KarolBagh','Karol01');
+insert into areamaster values('Delhi','HQDelhi01','KarolBagh','Karol02');
+insert into areamaster values('Delhi','HQDelhi01','KarolBagh','Karol03');
+insert into areamaster values('Delhi','HQDelhi01','KarolBagh','Karol04');
+insert into areamaster values('Delhi','HQDelhi01','KarolBagh','Karol05');
+
+insert into areamaster values('Delhi','HQDelhi01','Kalkaji','Kalkaji01');
+insert into areamaster values('Delhi','HQDelhi01','Kalkaji','Kalkaji02');
+insert into areamaster values('Delhi','HQDelhi01','Kalkaji','Kalkaji03');
+insert into areamaster values('Delhi','HQDelhi01','Kalkaji','Kalkaji04');
+insert into areamaster values('Delhi','HQDelhi01','Kalkaji','Kalkaji05');
+
+insert into areamaster values('Maharastha','HQMaharastha01','Pune','Pune01');
+insert into areamaster values('Maharastha','HQMaharastha01','Pune','Pune01');
+insert into areamaster values('Maharastha','HQMaharastha01','Pune','Pune01');
+insert into areamaster values('Maharastha','HQMaharastha01','Pune','Pune01');
+insert into areamaster values('Maharastha','HQMaharastha01','Pune','Pune01');
+
+insert into areamaster values('Maharastha','HQMaharastha01','Mumbai','Mumbai01');
+insert into areamaster values('Maharastha','HQMaharastha01','Mumbai','Mumbai02');
+insert into areamaster values('Maharastha','HQMaharastha01','Mumbai','Mumbai03');
+insert into areamaster values('Maharastha','HQMaharastha01','Mumbai','Mumbai04');
+insert into areamaster values('Maharastha','HQMaharastha01','Mumbai','Mumbai05');
+insert into areamaster values('Maharastha','HQMaharastha01','Mumbai','Mumbai06');
+insert into areamaster values('Maharastha','HQMaharastha01','Mumbai','Mumbai07');
+
+
+
+insert into areamaster values('Maharastha','HQMaharastha01','Aurangabad','Aura01');
+insert into areamaster values('Maharastha','HQMaharastha01','Aurangabad','Aura02');
+insert into areamaster values('Maharastha','HQMaharastha01','Aurangabad','Aura03');
+insert into areamaster values('Maharastha','HQMaharastha01','Aurangabad','Aura04');
+insert into areamaster values('Maharastha','HQMaharastha01','Aurangabad','Aura05');
+insert into areamaster values('Maharastha','HQMaharastha01','Aurangabad','Aura06');
+
+insert into areamaster values('TamilNadu','HQTamilNadu01','Chennai','Chennai01');
+insert into areamaster values('TamilNadu','HQTamilNadu01','Chennai','Chennai02');
+insert into areamaster values('TamilNadu','HQTamilNadu01','Chennai','Chennai03');
+insert into areamaster values('TamilNadu','HQTamilNadu01','Chennai','Chennai04');
+
+insert into areamaster values('TamilNadu','HQTamilNadu01','Erode','Erode01');
+insert into areamaster values('TamilNadu','HQTamilNadu01','Erode','Erode02');
+insert into areamaster values('TamilNadu','HQTamilNadu01','Erode','Erode03');
+insert into areamaster values('TamilNadu','HQTamilNadu01','Erode','Erode04');
+insert into areamaster values('TamilNadu','HQTamilNadu01','Erode','Erode05');
+insert into areamaster values('TamilNadu','HQTamilNadu01','Erode','Erode06');
+insert into areamaster values('TamilNadu','HQTamilNadu01','Erode','Erode07');
+
+insert into areamaster values('Bihar','HQBihar01','Patna','Patna01');
+insert into areamaster values('Bihar','HQBihar01','Patna','Patna02');
+insert into areamaster values('Bihar','HQBihar01','Patna','Patna03');
+insert into areamaster values('Bihar','HQBihar01','Patna','Patna04');
+insert into areamaster values('Bihar','HQBihar01','Patna','Patna05');
+insert into areamaster values('Bihar','HQBihar01','Patna','Patna06');
+insert into areamaster values('Bihar','HQBihar01','Patna','Patna07');
+insert into areamaster values('Bihar','HQBihar01','Patna','Patna08');
+
+insert into areamaster values('Bihar','HQBihar01','Samastipur','Samas01');
+insert into areamaster values('Bihar','HQBihar01','Samastipur','Samas02');
+insert into areamaster values('Bihar','HQBihar01','Samastipur','Samas03');
+insert into areamaster values('Bihar','HQBihar01','Samastipur','Samas04');
+insert into areamaster values('Bihar','HQBihar01','Samastipur','Samas05');
+
+
+
+insert into areamaster values('MadhyaPradesh','HQMadhyaPradesh01','Bhupal','Bhu11');
+insert into areamaster values('MadhyaPradesh','HQMadhyaPradesh01','Bhupal','Bhu12');
+insert into areamaster values('MadhyaPradesh','HQMadhyaPradesh01','Bhupal','Bhu18');
+insert into areamaster values('MadhyaPradesh','HQMadhyaPradesh01','Bhupal','Bhu19');
+
+insert into areamaster values('MadhyaPradesh','HQMadhyaPradesh01','Ujjain','Ujj00');
+insert into areamaster values('MadhyaPradesh','HQMadhyaPradesh01','Ujjain','Ujj10');
+insert into areamaster values('MadhyaPradesh','HQMadhyaPradesh01','Ujjain','Ujj20');
+insert into areamaster values('MadhyaPradesh','HQMadhyaPradesh01','Ujjain','Ujj30');
+insert into areamaster values('MadhyaPradesh','HQMadhyaPradesh01','Ujjain','Ujj40');
+
+insert into areamaster values('West Bengal','HQWest Bengal01','Kolkata','Kol01');
+insert into areamaster values('West Bengal','HQWest Bengal01','Kolkata','Kol02');
+insert into areamaster values('West Bengal','HQWest Bengal01','Kolkata','Kol03');
+insert into areamaster values('West Bengal','HQWest Bengal01','Kolkata','Kol04');
+insert into areamaster values('West Bengal','HQWest Bengal01','Kolkata','Kol05');
+insert into areamaster values('West Bengal','HQWest Bengal01','Kolkata','Kol06');
+insert into areamaster values('West Bengal','HQWest Bengal01','Kolkata','Kol07');
+insert into areamaster values('West Bengal','HQWest Bengal01','Kolkata','Kol08');
+
+insert into areamaster values('West Bengal','HQWest Bengal01','Siliguri','Sil01');
+insert into areamaster values('West Bengal','HQWest Bengal01','Siliguri','Sil02');
+insert into areamaster values('West Bengal','HQWest Bengal01','Siliguri','Sil03');
+insert into areamaster values('West Bengal','HQWest Bengal01','Siliguri','Sil04');
+insert into areamaster values('West Bengal','HQWest Bengal01','Siliguri','Sil05');
+
+insert into areamaster values('West Bengal','HQWest Bengal01','Durgapur','Durg01');
+insert into areamaster values('West Bengal','HQWest Bengal01','Durgapur','Durg02');
+insert into areamaster values('West Bengal','HQWest Bengal01','Durgapur','Durg03');
+insert into areamaster values('West Bengal','HQWest Bengal01','Durgapur','Durg04');
+insert into areamaster values('West Bengal','HQWest Bengal01','Durgapur','Durg05');
+insert into areamaster values('West Bengal','HQWest Bengal01','Durgapur','Durg06');
+
+insert into distancemaster values ('West Bengal','HQWest Bengal01','Durgapur','Durg01','Durg02',null,3.68);
+insert into distancemaster values ('West Bengal','HQWest Bengal01','Durgapur','Durg02','Durg03',null,5.70);
+insert into distancemaster values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Durg05',null,10.70);
+
+insert into distancemaster values('West Bengal','HQWest Bengal01','Siliguri','Sil01','Sil05',null,12.85);
+insert into distancemaster values('West Bengal','HQWest Bengal01','Siliguri','Sil02','Sil04',null,18.95);
+insert into distancemaster values('West Bengal','HQWest Bengal01','Siliguri','Sil01','Sil02',null,6.85);
+
+insert into distancemaster values('West Beagal','HQWest Bengal01','Kolkata','Kol01','Kol02',null,6.0);
+insert into distancemaster values('West Beagal','HQWest Bengal01','Kolkata','Kol02','Kol03',null,5.0);
+insert into distancemaster values('West Beagal','HQWest Bengal01','Kolkata','Kol03','Kol04',null,8.23);
+insert into distancemaster values('West Beagal','HQWest Bengal01','Kolkata','Kol04','Kol05',null,11.0);
+insert into distancemaster values('West Beagal','HQWest Bengal01','Kolkata','Kol03','Kol05',null,19.23);
+
+insert into degreemaster values('MBBS');
+insert into degreemaster values('FRCS');
+insert into degreemaster values('DMD');
+insert into degreemaster values('DDS');
+insert into degreemaster values('MD');
+insert into degreemaster values('DO');
+
+insert into specializationmaster values('Nerve ');
+insert into specializationmaster values('Cancer');
+insert into specializationmaster values('Chest');
+insert into specializationmaster values('Child ');
+insert into specializationmaster values('Diabetes');
+insert into specializationmaster values('ENT');
+insert into specializationmaster values('General');
+insert into specializationmaster values('Eye');
+insert into specializationmaster values('Heart');
+insert into specializationmaster values('Kidney');
+insert into specializationmaster values('Orthopedic');
+insert into specializationmaster values('Sexual');
+
+insert into chemistmaster values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Maa Kali Drugs','Abcd Road',to_date('22/01/1962','dd/mm/yyyy'),to_date('18/09/1991','dd/mm/yyyy'),'03324412092','9865475850','Y','M');
+insert into chemistmaster values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Modern Remedies','Pqrs Road',to_date('01/09/1990','dd/mm/yyyy'),to_date('25/11/1999','dd/mm/yyyy'),'03398752645','9335001002','Y','M');
+insert into chemistmaster values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Medi Life','XYZ Road',to_date('01/09/1990','dd/mm/yyyy'),to_date('25/11/1999','dd/mm/yyyy'),'03398752645','9335001002','Y','M');
+
+insert into stockistmaster values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Durgapur Drug Distributor','Abcd Road',to_date('22/01/1962','dd/mm/yyyy'),to_date('18/09/1991','dd/mm/yyyy'),'03324412092','9865475850','Y','M');
+
+
+insert into productcategorymaster values('Saleable');
+insert into productcategorymaster values('Gift');
+
+insert into productmaster values('Saleable','Enum2.5',35.00);
+insert into productmaster values('Saleable','Enum5.0',55.00);
+insert into productmaster values('Saleable','Stamlo2.5',45.00);
+insert into productmaster values('Saleable','Stamlo5.0',75.00);
+insert into productmaster values('Saleable','PazoPressXL',65.00);
+insert into productmaster values('Saleable','PazoPressXL2.5',75.00);
+insert into productmaster values('Saleable','PazoPressXL5.0',95.00);
+insert into productmaster values('Saleable','Aldo2.5',18.00);
+insert into productmaster values('Saleable','Aldo5.0',34.00);
+insert into productmaster values('Saleable','SupermetXL25',32.00);
+insert into productmaster values('Saleable','SupermetXL50',55.00);
+insert into productmaster values('Saleable','GlynezMF',9.85);
+insert into productmaster values('Saleable','Glynez5',19.85);
+insert into productmaster values('Saleable','Glynez10',9.85);
+insert into productmaster values('Saleable','AtoZGold',119.85);
+insert into productmaster values('Saleable','New Bonna Plus',89.85);
+insert into productmaster values('Saleable','Autrin Plus',69.85);
+insert into productmaster values('Saleable','Eye Site',1299.35);
+insert into productmaster values('Saleable','Ketlur Eye Drop',69.85);
+insert into productmaster values('Saleable','Eye Glow',189.25);
+insert into productmaster values('Saleable','Pantocid IT',99.85);
+insert into productmaster values('Saleable','Pan-40D',79.85);
+insert into productmaster values('Saleable','Eposis',1579.85);
+insert into productmaster values('Saleable','Vintron',750.85);
+insert into productmaster values('Saleable','Lasix-40',9.85);
+insert into productmaster values('Saleable','Viagra',579.85);
+
+insert into doctormaster values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Mr Amitav Bacchan',to_date('13/05/1958','dd/mm/yyyy'),'M','A','abcd road',to_date('22/09/1988','dd/mm/yyyy'),'9836644587','0334402586','Y','FRCS','Diabetes','Prospective');
+insert into doctormaster values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Ms Mallika Sherawat',to_date('19/11/1989','dd/mm/yyyy'),'F','A','abcd road',to_date('22/09/2011','dd/mm/yyyy'),'9831144500','03324433158','Y','MD','Sexual','Interesting');
+insert into doctormaster values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Mr Al Pacheno',to_date('29/09/1958','dd/mm/yyyy'),'M','B','xyz road',to_date('02/01/1987','dd/mm/yyyy'),'7891144500','033211475689','Y','DND','Kidney','Superb');
+
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Mr Amitav Bacchan','Enum2.5');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Mr Amitav Bacchan','Stamlo2.5');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Mr Amitav Bacchan','Stamlo5.0');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Mr Amitav Bacchan','Glynez5');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Mr Amitav Bacchan','Glynez10');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Ms Mallika Sherawat','Viagra');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Ms Mallika Sherawat','Ketlur Eye Drop');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Ms Mallika Sherawat','Eye Glow');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Ms Mallika Sherawat','Pantocid IT');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Ms Mallika Sherawat','Pan-40D');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Mr Al Pacheno','AtoZGold');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Mr Al Pacheno','New Bonna Plus');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Mr Al Pacheno','Autrin Plus');
+insert into docproducts values('West Bengal','HQWest Bengal01','Durgapur','Durg01','Mr Al Pacheno','Eye Site');
+
+insert into groupmaster values('ASM','HQWest Bengal01');
+insert into groupmaster values('ZSM','HQWest Bengal01');
+insert into groupmaster values('MR','HQWest Bengal01');
+
+insert into usermaster values('MR','HQWest Bengal01','West Bengal','bong02','ur101g','Xyz','Ray','abcdefgh',to_date('25/06/1990','dd/mm/yyyy'),'abcd road','abcd raod','03324411992','9831166421','M','N',null,to_date('01/02/2012','dd/mm/yyyy'),12000,5400,0.0,0.0,0.0,0.0,'A');
+
+
+
+
+
+
---
+++ sql files/sir_database_pharma.sql
@@ -0,0 +1,69 @@
+
+create table statemaster(statename varchar2(30) constraint pk_statemaster primary key);
+
+create table hqmaster(statename varchar2(30),hqname varchar2(30), constraint pk_hqmaster primary key(statename,hqname));
+
+alter table hqmaster add constraint fk_hqmaster foreign key(statename) references statemaster(statename);
+
+create table citymaster(statename varchar2(30),hqname varchar2(30),cityname varchar2(30),constraint pk_citymaster primary key (statename,hqname,cityname));
+
+alter table citymaster add constraint fk_citymaster foreign key(hqname,statename) references hqmaster(hqname,statename);
+
+
+create table areamaster(statename varchar2(30),hqname varchar2(30),cityname varchar2(30),areaname varchar2(20),constraint pk_areamaster primary key(statename,hqname,cityname,areaname));
+
+alter table areamaster add constraint fk_areamaster foreign key(hqname,statename,cityname) references citymaster(hqname,statename,cityname);
+
+create table distancemaster(statename varchar2(30),hqname varchar2(30),cityname varchar2(30),from_area varchar2(30),to_area varchar2(30),fromarea varchar2(30),distance number(12,2));
+
+
+
+create table degreemaster(degreename varchar2(30) constraint pk_degreemaster primary key );
+
+create table specializationmaster(spl varchar2(30) constraint pk_specialization primary key);
+
+create table chemistmaster(stateCode varchar2(50),hqcode varchar2(50),citycode varchar2(50),areacode varchar2(50),name varchar2(50),addr varchar2(50),dob date,doa date,phone varchar2(50),mobile varchar2(50),marital char(1) check(marital in('Y','y','N','n')),sex char(1) check (sex in ('M','m','F','f')),constraint pk_chemistmaster primary key(statecode,hqcode,citycode,areacode,name));
+
+create table stockistmaster(statecode varchar2(40),hqcode varchar2(50),citycode varchar2(50),areacode varchar2(50),name varchar2(50),addr varchar2(50),dob date,doa date,phone varchar2(50),mobile varchar2(50),marital char(1) check(marital in('Y','y','N','n')), sex char(1) check (sex in('M','m','F','f')),constraint pk_stockistmaster primary key(statecode,hqcode,citycode,areacode,name));
+
+
+create table productcategorymaster(cat_name varchar2(20) primary key);
+
+create table productmaster(productCategory varchar2(50),productName varchar2(50) primary Key,price Number(15,2));
+
+
+create table doctormaster(statename varchar2(50),hqname varchar2(50),cityname varchar2(50),areaname varchar2(50),docname varchar2(50),dob date,
+sex char(1) check(sex in('M','m','F','f')),grade  char(1) check(grade in ('A','B','C','O')),address varchar2(50),doa date,mobile varchar2(50),phoneno varchar2(50),marital char(1) check(marital in ('Y','y','N','n')),degree varchar2(50),specialization varchar2(50),remarks varchar2(200),constraint pk_doctormaster primary key(statename,hqname,cityname,areaname,docname));
+
+alter table doctormaster add constraint fk_doctormaster foreign key(hqname,statename,cityname,areaname) references areamaster(hqname,statename,cityname,areaname);
+
+
+create table docproducts(statename varchar2(50),hqname varchar2(50),cityname varchar2(50),areaname varchar2(50),docname varchar2(50),productname varchar2(50), constraint pk_docproduct primary key(statename,hqname,cityname,areaname,docname,productname));
+
+alter table docproducts add constraint fk_docproduct foreign key(hqname,statename,cityname,areaname,docname) references doctormaster(hqname,statename,cityname,areaname,docname);
+
+create table groupmaster(groupname varchar2(50),hqname varchar2(50),constraint pk_groupmaster primary key(hqname,groupname)); 
+
+
+
+create table usermaster(groupname varchar2(50),hqname varchar2(50),statename varchar2(50),loginid varchar2(20),loginpassword varchar2(20),firstname varchar2(50),lastname varchar2(50),fathername varchar2(50),dob date,mailaddress varchar2(50),parmanentaddress varchar2(50),phonenumber varchar2(50),mobilenumber varchar2(50),sex char(1) check(sex in('M','m','F','f')),maritalstatus char(1) check(maritalstatus in ('Y','y','N','n')),doa date,doj date,basic number(15,2),hra number(15,2),eallowance number(15,2),communication number(15,2),vma number(15,2),cca number(15,2),userstatus char(1) check(userstatus in ('A','D')));
+
+
+create table doctor_dcr(groupname varchar2(50),hqname varchar2(50),statename varchar2(50),loginid varchar2(20),dcrdate date,targetdcrdate date,latedcr char(2),areacode varchar2(50),docname varchar2(50),visittype char(1),visitStatus char(1),businessstatus Number(9),prodstatus char(1));
+
+create table docdcrproduct(groupname varchar2(50),hqname varchar2(50),statename varchar2(50),loginid varchar2(20),dcrdate Date,productname varchar2(50),qty Number(5),sample char(1),doctorremark varchar2(200));
+
+
+create table chemistdcr(groupname varchar2(50),hqname varchar2(50),statename varchar2(50),loginID varchar2(20),dcrdate date,targetdcrdate date,latedcr char(5),
+areacode varchar2(50),name varchar2(50),ordernumber number(10),orderamount number(15,2));
+
+
+
+create table expense_mr(groupname varchar2(50),hqname varchar2(50),statename varchar2(50),loginid varchar2(20),dcrdate date,targetdcrdate date,
+latedcr char(2),areacode varchar2(50),expensetype char(1),amt number(15,2),remarks varchar2(200),approved char(4));
+
+
+create table primarytarget(groupname varchar2(50),hqname varchar2(50),statename varchar2(50),areacode varchar2(50),loginid varchar2(20),productname varchar2(50),mthlastdate date,qty number(15,2));
+
+
+
